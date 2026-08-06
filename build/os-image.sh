e0cd90a8d8778514b543f124a0371a4e2c8868c9
wippsanrinthailand80-commits <wippsanrinthailand80-commits@users.noreply.github.com>
2026-08-06 00:20:32 +0000

    fix(plymouth): include bochs-drm framebuffer modules + explicit plymouthd.conf + diagnostic bootargs

--- build/os-image.sh
+++ build/os-image.sh
@@ -42,7 +42,7 @@ lb config \
   --binary-images iso-hybrid \
   --mode ubuntu \
   --archive-areas "main restricted universe multiverse" \
-  --bootappend-live "boot=live config quiet splash console=tty0 console=ttyS0,115200n8" \
+  --bootappend-live "boot=live config quiet splash loglevel=7 plymouth:debug console=tty0 console=ttyS0,115200n8" \
   --linux-flavours generic \
   --initramfs live-boot \
   --initsystem systemd \
